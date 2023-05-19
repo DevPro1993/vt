da8af26f1f797a68e27c4afe5e86114eff810dad
Apoorva Lele <apoorv.lele93@gmail.com>
2023-05-19 13:31:17 +0530

    added divergence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,41 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'vt';
-  mode: string = 'easy';
-  maxSpacer = 290;
+  difficulty: string = 'easy';
+  mode: 'convergence' | 'divergence' = 'convergence';
+  maxSpacer: number;
   minSpacer = 100;
   spacer = this.minSpacer;
   jump = 15;
 
   ngOnInit(): void {
-    
+    this.setDifficulty('easy')
   }
 
   moveSpacer(event: KeyboardEvent) {
-   const key = event.code;
-   if (key === 'ArrowUp') {
-    if (this.spacer < this.maxSpacer - this.jump) {
-      this.spacer += this.jump
-    } else {
-      this.spacer = this.maxSpacer;
+    const key = event.code;
+    if (key === 'ArrowUp') {
+      if (this.spacer < this.maxSpacer - this.jump) {
+        this.spacer += this.jump
+      } else {
+        this.spacer = this.maxSpacer;
+      }
+    } else if (key === 'ArrowDown') {
+      if (this.spacer > this.minSpacer + this.jump) {
+        this.spacer -= this.jump
+      } else {
+        this.spacer = this.minSpacer
+      }
+    } else if (key === 'Space') {
+      const diff = this.maxSpacer - this.minSpacer;
+      const randomJump = Math.random() * diff;
+      this.spacer = randomJump + this.minSpacer;
     }
-   } else if (key === 'ArrowDown') {
-    if (this.spacer > this.minSpacer + this.jump) {
-      this.spacer -= this.jump
-    } else {
-      this.spacer = this.minSpacer
-    }
-   } else if (key === 'Space') {
-    const diff = this.maxSpacer - this.minSpacer;
-    const randomJump = Math.random() * diff;
-    this.spacer = randomJump + this.minSpacer;
-   }
   }
 
-  setMaxSpacer(data: string) {
-    if (this.mode === 'easy') this.maxSpacer = 190
-    if (this.mode === 'intermediate') this.maxSpacer = 290
-    if (this.mode === 'difficult') this.maxSpacer = 390
+  setDifficulty(data: string) {
+    if (data === 'easy') this.maxSpacer = 190
+    if (data === 'intermediate') this.maxSpacer = 290
+    if (data === 'difficult') this.maxSpacer = 390
+    this.reset()
+  }
+
+  setMode(data: string) {
+    this.mode = data === 'convergence' ? 'convergence' : 'divergence';
     this.reset()
   }
 
